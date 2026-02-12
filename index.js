3f7b5aa1d1f2506e7df17bd2b519c29c9ee7eb05
yukta.nagpal <yukta.nagpal@servicenow.com>
2026-02-12 13:41:16 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res)=> {
+    res.send("Hello World")
+})
+
+app.get('/youtube', (req, res) => {
+    res.send("Chai aur Code")
+})
+
+app.listen(process.env.PORT, (req, res) => {
+    console.log(`Example port listening at ${process.env.PORT}`)
+})
